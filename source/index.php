9a35736bbe5988ab35e1911472fa7753132ef2db
Param Siddharth <30315706+paramsiddharth@users.noreply.github.com>
2020-01-28 22:34:32 +0530

    Add files via upload

---
+++ source/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<link rel="shortcut icon" href="favicon.ico"/>
+<meta charset="UTF-8"/>
+<meta type="description" content="The unofficial confession-submission site for JUET, Guna."/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<?php
+require "info.php";
+?>
+<title>Confessions@JUET: Let your voice be heard!</title>
+<style>
+<!--
+
+body {
+	background-color: #87CEFA;
+	
+}
+
+#submit {
+	background-color: #E0E0F0;
+	transition: .2s;
+	-webkit-transition: .2s;
+	text-decoration: none;
+	color: #000000;
+	font-size: 20px; 
+	font-weight: medium;
+	border-radius: 9px;
+}
+
+#submit:hover {
+	background-color: #3F2020;
+	transition: .2s;
+	-webkit-transition: .2s;
+	font-size: 23px;
+	text-shadow: 0px 0px 3px #FFFFFF;
+	color: #FFFFFF;
+}
+
+a.s {
+	transition: .2s;
+	-webkit-transition: .2s;
+	text-decoration: none;
+	color: #220000;
+	text-shadow: 0px 0px 1px #000000;
+	font-weight: medium;
+	border-radius: 9px;
+}
+
+a:hover {
+	background-color: #3F2020;
+	transition: .2s;
+	-webkit-transition: .2s;
+	padding: 4px;
+	text-shadow: 0px 0px 3px #FFFFFF;
+	color: #FFFFFF;
+}
+
+header {
+	text-align: center;
+	background-color: #E8E8FF;
+	border-radius: 0px 0px 5px 5px;
+}
+
+h1 {
+	height: 18px;
+	font-size: 40px;
+	text-shadow: 1px 1px 1px #000000;
+	margin-top: 3px;
+	transition: .5s;
+	-webkit-transition: .5s;
+}
+
+h1:hover {
+	transition: 5s;
+	-webkit-transition: 5s;
+	color: #777777
+}
+
+#main {
+	text-align: center;
+	margin-top: 10px;
+	margin-bottom: 13px;
+	background-color: #FFFFFF;
+	padding-top: 14px;
+	padding-bottom: 10px;
+	width: 70%;
+	margin-left: auto;
+	margin-right: auto;
+	border-radius: 10px;
+}
+
+-->
+</style>
+
+</head>
+<body>
+<header>
+<br/>
+<img src="header.jpg" style="border-radius: 13px; display: block; margin-left: auto; margin-right: auto; width: 90%;"/>
+<h1>Confessions@JUET</h1>
+<span style="font-style: italic; font-weight: light; font-weight: bold;">Let your voice be heard!</span>
+</header>
+<div style="text-align: center; padding-top: 6px;">
+<?php
+echo "You confession will be uploaded to our <a href=\"$insta\" class=\"s\">Instagram page</a>. Be patient! It might take a while to get it done.";
+?>
+</div>
+<div id="main">
+<form action="submit.php" method="post">
+<span style="font-size: 20px;">Enter your confession: </span>
+<br/>
+<textarea name="confession" style="width: 60%; height: 100px; border-radius: 6px; font-size: 18px;" placeholder="I would like to confess..." required></textarea>
+<br/><br/>
+<input type="submit" id="submit" name="submit" value="Submit!"/>
+</div>
+<footer><hr style="color: #FFFFFF;"/>
+<div style="text-align: center; word-spacing: 7px;">
+<?php
+echo "<a href=\"index.php\">Home</a> | <a href=\"about.php\">About</a> | <a href=\"$insta\">Instagram</a> | <a href=\"http://juet.ac.in\">JUET</a> | <a href=\"http://webkiosk.juet.ac.in\">Webkiosk</a>";
+?>
+</div>
+</footer>
+</body>
+</html>
---
+++ source/reveal.php
@@ -0,0 +1,4 @@
+<?php
+require "info.php";
+echo "<form action=\"view.php\" method=\"post\"><span style=\"font-size: 20px;\">Password </span><input type=\"password\" name=\"passin\" placeholder=\"Password\" required></input><input type=\"submit\" id=\"submit\" name=\"submit\" value=\"Submit!\"/></div>";
+?>
